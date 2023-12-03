d1e92a47f2f28f3679ee6c27283c9a5b3217decb
Jesús Fernández Jiménez <jesusfj710@proton.me>
2023-12-03 14:07:10 +0100

    First functional menu

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,5 +1,6 @@
 <template>
-  <header class="p-4">
+  <header class="grow p-4 flex place-content-center">
+    <SideMenu />
     <h1
       class="text-center text-xl underline decoration-green-500 font-bold dark:text-black decoration-double drop-shadow-[0_2px_2px_rgba(255,255,255,0.9)] dark:drop-shadow-[0_2px_2px_rgba(0,0,0,0.5)]"
     >
---
+++ components/SideMenu.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="absolute top-0 left-0 py-4 px-2 xs:p-4">
+    <UButton
+      variant="solid"
+      icon="i-heroicons-bars-3"
+      class="rounded-xl backdrop-blur-xl bg-white/50 dark:bg-black/30 hover:ring-2 ring-primary-500/[.55]"
+    />
+  </div>
+</template>
